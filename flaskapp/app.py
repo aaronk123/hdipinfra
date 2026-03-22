5fcd365406014de24296a661c5a42a9d4d9cf161
Aaron Kelleher <33543096+aaronk123@users.noreply.github.com>
2026-03-22 22:28:32 +0000

    Add files via upload

---
+++ flaskapp/app.py
@@ -0,0 +1,62 @@
+from flask import Flask
+import psycopg2
+import os
+ 
+app = Flask(__name__)
+ 
+@app.route("/")
+def home():
+	return "Infrastructure Automation Demo"
+ 
+@app.route("/health")
+def health():
+	return "ok"
+ 
+@app.route("/db")
+def db_test():
+	try:
+		conn = psycopg2.connect(
+			host="192.168.178.134",
+			database="appdb",
+			user="appuser",
+			password="apppassword"
+		)
+		conn.close()
+		return "database connection successful"
+	except Exception as e:
+		return str(e)
+ 
+@app.route("/transaction")
+def transaction_test():
+    try:
+        conn = psycopg2.connect(
+            host=os.environ.get("DB_HOST"),
+            database=os.environ.get("DB_NAME"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASS")
+        )
+        cur = conn.cursor()
+ 
+        # insert hardcoded value
+        cur.execute(
+            "INSERT INTO messages (content) VALUES (%s)",
+            ("deployment_success",)
+        )
+ 
+        # read newest value
+        cur.execute(
+            "SELECT id, content FROM messages ORDER BY id DESC LIMIT 1"
+        )
+        row = cur.fetchone()
+ 
+        conn.commit()
+        cur.close()
+        conn.close()
+ 
+        return f"Inserted + Read -> {row[0]} : {row[1]}"
+ 
+    except Exception as e:
+        return str(e)
+ 
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=5000)
